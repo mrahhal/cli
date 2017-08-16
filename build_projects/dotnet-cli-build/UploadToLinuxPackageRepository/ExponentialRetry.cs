da2745521c39dead657f22ff32fddec6b3927f2b
William Li <wul@microsoft.com>
2017-08-16 11:32:35 -0700

    Separate the files

--- build_projects/dotnet-cli-build/UploadToLinuxPackageRepository/ExponentialRetry.cs
+++ build_projects/dotnet-cli-build/UploadToLinuxPackageRepository/ExponentialRetry.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Microsoft.DotNet.Cli.Build
+namespace Microsoft.DotNet.Cli.Build.UploadToLinuxPackageRepository
 {
     public static class ExponentialRetry
     {
